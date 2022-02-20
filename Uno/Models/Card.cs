8b992a0b144596696ee6def1309d09d20d53de41
Edward Knueppel <edward.knueppel@zywave.com>
2022-02-19 21:29:21 -0600

    Add project files.

---
+++ Uno/Models/Card.cs
@@ -0,0 +1,17 @@
+﻿namespace Uno.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class Card
+    {
+        public string Color { get; init; }
+        public int? Number { get; init; }
+        public CardType CardType { get; init; }
+
+
+    }
+}
---
+++ Uno/Models/CardType.cs
@@ -0,0 +1,18 @@
+﻿namespace Uno.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal enum CardType
+    {
+        Wild,
+        WildDrawFour,
+        Number,
+        Skip,
+        DrawTwo,
+        Reverse
+    }
+}
